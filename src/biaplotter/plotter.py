74c22d24ec5484af5094131ff71b85757385ef8f
Marcelo Zoccoler <marcelozoc@gmail.com>
2024-04-11 13:34:33 +0200

    replace get_active_artist by setter method and add documentation

--- src/biaplotter/plotter.py
+++ src/biaplotter/plotter.py
@@ -1,17 +1,20 @@
-import numpy as np
+from __future__ import annotations
+
 from pathlib import Path
 from enum import Enum, auto
 from nap_plot_tools import CustomToolbarWidget, QtColorSpinBox, make_cat10_mod_cmap
 from napari.layers import Labels, Points, Tracks
 from napari_matplotlib.base import SingleAxesWidget
 from napari_matplotlib.util import Interval
-from qtpy.QtWidgets import QHBoxLayout, QLabel
+from qtpy.QtWidgets import QHBoxLayout, QLabel, QWidget
 from psygnal import Signal
-from qtpy.QtCore import Qt
+from typing import Union, TYPE_CHECKING, Optional
 
-# from biaplotter.selectors import CustomLassoSelector
 from biaplotter.artists import Scatter, Histogram2D
 from biaplotter.selectors import InteractiveRectangleSelector, InteractiveEllipseSelector, InteractiveLassoSelector
+
+if TYPE_CHECKING:
+    import napari
 
 icon_folder_path = (
     Path(__file__).parent / "icons"
@@ -30,6 +33,69 @@ class SelectorType(Enum):
 
 
 class CanvasWidget(SingleAxesWidget):
+    """A widget that contains a canvas with matplotlib axes and a selection toolbar.
+
+    The widget includes a selection toolbar with buttons to enable/disable selection tools.
+    The selection toolbar includes a color class spinbox to select the class to assign to selections.
+    The widget includes artists and selectors to plot data and select points.
+
+    Parameters
+    ----------
+    napari_viewer : napari.viewer.Viewer
+        The napari viewer.
+    parent : QWidget, optional
+        The parent widget, by default None.
+    label_text : str, optional
+        The text to display next to the class spinbox, by default "Class:".
+
+    Attributes
+    ----------
+    n_layers_input : Interval
+        Amount of available input layers.
+    input_layer_types : tuple
+        All layers that have a .features attributes.
+    selection_tools_layout : QHBoxLayout
+        The selection tools layout.
+    selection_toolbar : CustomToolbarWidget
+        The selection toolbar.
+    class_spinbox : QtColorSpinBox
+        The color class spinbox.
+    colormap : matplotlib.colors.ListedColormap
+        The colormap to use for the color class spinbox.
+    artists : dict
+        Dictionary of artists.
+    selectors : dict
+        Dictionary of selectors.
+    _active_artist : Union[Scatter, Histogram2D]
+        Stores the active artist.
+
+    Properties
+    ----------
+    active_artist : Union[Scatter, Histogram2D]
+        Returns the active artist.
+
+    Methods
+    -------
+    _build_selection_toolbar_layout(label_text: str = "Class:")
+        Builds the selection toolbar layout.
+    on_enable_selector(checked: bool)
+        Enables or disables the selected selector.
+    add_artist(artist_type: ArtistType, artist_instance: Union[Scatter, Histogram2D], visible: bool=False)
+        Adds a new artist instance to the artists dictionary.
+    add_selector(selector_type: SelectorType, selector_instance: Union[InteractiveRectangleSelector, InteractiveEllipseSelector, InteractiveLassoSelector])
+        Adds a new selector instance to the selectors dictionary.
+
+    Signals
+    -------
+    artist_changed_signal : Signal
+        Signal emitted when the current artist changes.
+
+    Signals and Slots
+    -----------------
+    This class automatically connects the `data_changed_signal` signal from each artist to the `update_data` slot in each selector.
+    This allows artists to notify selectors when the data changes. Selectors can then synchronize their data with the artist's data.
+
+    """
     # Amount of available input layers
     n_layers_input = Interval(1, None)
     # All layers that have a .features attributes
@@ -37,7 +103,7 @@ class CanvasWidget(SingleAxesWidget):
     # # Signal emitted when the current artist changes
     artist_changed_signal = Signal(ArtistType)
 
-    def __init__(self, napari_viewer, parent=None, label_text="Class:"):
+    def __init__(self, napari_viewer: "napari.viewer.Viewer", parent: Optional[QWidget] = None, label_text: str = "Class:"):
         super().__init__(napari_viewer, parent=parent)
 
         # Add selection tools layout below canvas
@@ -79,12 +145,13 @@ class CanvasWidget(SingleAxesWidget):
         self.layout().insertLayout(2, self.selection_tools_layout)
 
         # Create artists
+        self._active_artist = None
         self.artists = {}
         self.add_artist(ArtistType.SCATTER, Scatter(
             ax=self.axes, colormap=self.colormap))
         self.add_artist(ArtistType.HISTOGRAM2D, Histogram2D(ax=self.axes))
         # Set histogram2d as the default artist
-        self.set_active_artist(ArtistType.HISTOGRAM2D)
+        self.active_artist = self.artists[ArtistType.HISTOGRAM2D]
 
         # Create selectors
         self.selectors = {}
@@ -99,7 +166,22 @@ class CanvasWidget(SingleAxesWidget):
             for selector in self.selectors.values():
                 artist.data_changed_signal.connect(selector.update_data)
 
-    def _build_selection_toolbar_layout(self, label_text="Class:"):
+    def _build_selection_toolbar_layout(self, label_text: str = "Class:"):
+        """Builds the selection toolbar layout.
+
+        The toolbar starts without any buttons. Add buttons using the add_custom_button method.
+        The toolbar includes a color class spinbox to select the class to assign to selections.
+
+        Parameters
+        ----------
+        label_text : str, optional
+            The text to display next to the class spinbox, by default "Class:"
+
+        Returns
+        -------
+        QHBoxLayout
+            The selection toolbar layout.
+        """
         # Add selection tools layout below canvas
         selection_tools_layout = QHBoxLayout()
         # Add selection toolbar
@@ -113,9 +195,17 @@ class CanvasWidget(SingleAxesWidget):
         selection_tools_layout.addStretch(1)
         return selection_tools_layout
 
-    def on_enable_selector(self, checked):
+    def on_enable_selector(self, checked: bool):
+        """Enables or disables the selected selector.
+
+        Enabling a selector disables all other selectors.
+
+        Parameters
+        ----------
+        checked : bool
+            Whether the button is checked or not.
+        """
         sender_name = self.sender().text()
-        # print(sender_name)
         if checked:
             # If the button is checked, disable all other buttons
             for button_name, button in self.selection_toolbar.buttons.items():
@@ -133,45 +223,67 @@ class CanvasWidget(SingleAxesWidget):
             for selector_type, selector in self.selectors.items():
                 if selector_type.name == sender_name:
                     selector.remove()
-            
 
-    # def on_current_artist_changed(self, artist_type):
-    #     self.current_artist_changed_signal.emit(artist_type)
+    @property
+    def active_artist(self):
+        """Returns the active artist."""
+        return self._active_artist
 
-    def get_active_artist(self):
+    @active_artist.setter
+    def active_artist(self, value: Union[Scatter, Histogram2D]):
+        """Sets the active artist.
+
+        Makes the selected artist visible and all other artists invisible.
+
+        Parameters
+        ----------
+        value : Artist
+            The new artist to set as active.
+
+        Signals
+        -------
+        artist_changed_signal : Signal
+            Signal emitted when the current artist changes.
+        """
+        self._active_artist = value
         for artist in self.artists.values():
-            if artist.visible:
-                return artist
-
-    def set_active_artist(self, new_artist_type):
-        for artist_type, artist in self.artists.items():
-            if artist_type == new_artist_type:
+            if artist == self._active_artist:
                 artist.visible = True
             else:
                 artist.visible = False
+        # Gets artist type
+        for artist_type, artist in self.artists.items():
+            if artist == value:
+                active_artist_type = artist_type
         # Emit signal to notify that the current artist has changed
-        self.artist_changed_signal.emit(new_artist_type)
+        self.artist_changed_signal.emit(active_artist_type)
 
-    def add_artist(self, artist_type, artist_instance, visible=False):
+    def add_artist(self, artist_type: ArtistType, artist_instance: Union[Scatter, Histogram2D], visible: bool = False):
         """
         Adds a new artist instance to the artists dictionary.
 
-        Parameters:
-        - artist_type (ArtistType): The type of the artist, defined by the ArtistType enum.
-        - artist_instance: An instance of the artist class.
+        Parameters
+        ----------
+        artist_type : ArtistType
+            The type of the artist, defined by the ArtistType enum.
+        artist_instance : Union[Scatter, Histogram2D]
+            An instance of the artist class.
         """
         if artist_type in self.artists:
             raise ValueError(f"Artist '{artist_type.name}' already exists.")
         self.artists[artist_type] = artist_instance
         artist_instance.visible = visible
 
-    def add_selector(self, selector_type, selector_instance):
+    def add_selector(self, selector_type: SelectorType, selector_instance: Union[InteractiveRectangleSelector, InteractiveEllipseSelector, InteractiveLassoSelector]):
         """
         Adds a new selector instance to the selectors dictionary.
 
-        Parameters:
-        - selector_type (SelectorType): The type of the selector, defined by the SelectorType enum.
-        - selector_instance: An instance of the selector class.
+        Parameters
+        ----------
+        selector_type : SelectorType
+            The type of the selector, defined by the SelectorType enum.
+        selector_instance : Union[InteractiveRectangleSelector, InteractiveEllipseSelector, InteractiveLassoSelector]
+            An instance of the selector class.
         """
         if selector_type in self.selectors:
             raise ValueError(
