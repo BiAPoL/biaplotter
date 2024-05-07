ed7aba208178e53ce370d1561301f4dc9dca8228
Marcelo Zoccoler <marcelozoc@gmail.com>
2024-05-07 09:59:01 +0200

    bump version

--- src/biaplotter/__init__.py
+++ src/biaplotter/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 from .plotter import CanvasWidget
 from .artists import Scatter, Histogram2D
 from .selectors import InteractiveRectangleSelector, InteractiveEllipseSelector, InteractiveLassoSelector
