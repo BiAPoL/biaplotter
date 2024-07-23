5b5f420ab9f171e7d196a4c121cf900d85fa602b
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2024-07-23 14:18:21 +0200

    reset alpha if new data is added

--- src/biaplotter/artists.py
+++ src/biaplotter/artists.py
@@ -169,7 +169,8 @@ class Scatter(Artist):
         else:
             # If the scatter plot already exists, just update its data
             self._scatter.set_offsets(value)
-        
+            self._scatter.set_alpha(1)
+
         if self._color_indices is None:
             self.color_indices = 0  # Set default color index
         else:
